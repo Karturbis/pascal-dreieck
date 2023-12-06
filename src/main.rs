b220f3b7ebcd02e4589592402d6e2109e13ebec3
karturbis <vulkanera42@gmx.ch>
2023-12-06 13:17:12 +0100

    add function to calculate Fakultäten

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    let number:u128 = 16;
+    fakultaet(number)
+}
+
+/**
+Berechnet die Fakultaet der uebergebenen Zahl.
+*/
+fn fakultaet(faknumber:u128){
+
+    let mut result:u128 = 1;
+    let mut i:u128 = 1;
+    /*
+    Fakultäten:
+    0! = 1          = 1
+    1! = 1          = 1
+    2! = 1*2        = 2
+    3! = 1*2*3      = 6
+    4! = 1*2*3*4    = 24
+    5! = 1*2*3*4*5  = 120
+
+    daher x*(x-1)! = x!
+    */ 
+    while i <= faknumber{
+        
+        result = result * i;
+
+        i +=1;
+    }
+    println!("Die Fakultät der Nummer ist: {}", result);
+
 }
